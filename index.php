e4f35d60c55b5309431c7e36a8bc84a80d54798d
Kyle <kocfreed@gmail.com>
2019-05-02 09:52:27 -0700

    CSS is awesome.

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="/aesthetic.css">
+        <script src="jquery-3.4.0.min.js"></script>
         <title></title>
     </head>
     <body>
