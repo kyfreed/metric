def1764ebad62a34b1951fe4049cde69c6132982
Kyle <kocfreed@gmail.com>
2019-05-02 18:58:49 -0700

    Start adding other pages

---
+++ easier.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="/aesthetic.css">
+        <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+        <script>
+            $("document").ready(function(){
+                $("#display").click(function(){
+                    $("#panel").slideDown();
+                });
+            });
+        </script>
+        <title>The Metric System Is Easier To Use</title>
+    </head>
+    <body>
+        <h1>Easier to use</h1>
+        <p>The metric system is based in tens.</p>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -9,13 +9,27 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="/aesthetic.css">
-        <script src="jquery-3.4.0.min.js"></script>
-        <title></title>
+        <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+        <script>
+            $("document").ready(function(){
+                $("#display").click(function(){
+                    $("#panel").slideDown();
+                });
+            });
+        </script>
+        <title>The Metric System</title>
     </head>
     <body>
-        <div id="panel">
         <h1>The Metric System</h1>
         <p>Have you ever asked why there are 5280 feet in a mile? Well, there is an answer, but it is still inconvenient to do this. I believe that the US should officially switch to the metric system.</p>
-        </div>
+        <button class="btn btn-primary" id="display">Display links</button>
     </body>
+    <div id="panel">
+        <p> The metric system is</p>
+        <br>
+        <button class="btn btn-primary" onclick="window.location.href = '/easier.php'">Easier to use</button>
+    </div>
 </html>
